1b9158b88c00f801fdc84cc5411d6c6b9e429d00
moetez <mo3tez@gmail.com>
2018-07-07 15:02:51 +0100

    fixed files names

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-//import './index.css'
 import './index.scss'
 import 'font-awesome/css/font-awesome.min.css';
 
