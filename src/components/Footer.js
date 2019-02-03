4c4d04d13e6068e35acfe3d8f0b047152fcc2e82
moetez <mo3tez@gmail.com>
2019-02-03 19:45:51 +0100

    update packages and refactor layout.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => (
   <footer className="footer">
   <div className="content has-text-centered">
     <p>
-    Built with <span style={{color:'#8B0000'}}>❤️</span> using <a href="https://www.gatsbyjs.org/">Gatsby</a>.
+    Built with <span role ='img' style={{color:'#8B0000'}}>❤️</span> using <a href="https://www.gatsbyjs.org/">Gatsby</a>.
     <br/>
     View the source code on <a href="https://github.com/moetezch/moetez.me">Github</a>.
     </p>
