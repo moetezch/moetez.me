da63e76a27cc13adba23ccdc30251feb985532f9
moetez <mo3tez@gmail.com>
2019-02-03 21:31:46 +0100

    Import graphql from Gatsby

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 import Layout from '../../components/Layout'
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,60 +1,64 @@
-import React, { Component } from "react"
-import Img from "gatsby-image"
+import React, { Component } from 'react'
+import Img from 'gatsby-image'
 import SocialShare from '../components/SocialShare'
 import Layout from '../components/Layout'
-
+import { graphql } from 'gatsby'
 class PostTemplate extends Component {
-    render() {
-
-        const post = this.props.data.wordpressPost
-        let resolutions
-        if (post.featured_media) {
-            resolutions = post.featured_media.localFile.childImageSharp.resolutions
-        } return (
-            <Layout>
-            <div className="content has-text-centered container">
-                <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
-                {resolutions &&
-                    <div>
-                        <Img resolutions={resolutions} />
-
-                    </div>
-                }
-                <div dangerouslySetInnerHTML={{ __html: post.content }} />
-                <div >
-                <SocialShare title={this.props.data.wordpressPost.title} url={this.props.data.site.siteMetadata.siteUrl + this.props.location.pathname}/>
-                </div>
-                </div>
-            </Layout>
-        )
+  render() {
+    const post = this.props.data.wordpressPost
+    let resolutions
+    if (post.featured_media) {
+      resolutions = post.featured_media.localFile.childImageSharp.resolutions
     }
+    return (
+      <Layout>
+        <div className="content has-text-centered container">
+          <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+          {resolutions && (
+            <div>
+              <Img resolutions={resolutions} />
+            </div>
+          )}
+          <div dangerouslySetInnerHTML={{ __html: post.content }} />
+          <div>
+            <SocialShare
+              title={this.props.data.wordpressPost.title}
+              url={
+                this.props.data.site.siteMetadata.siteUrl +
+                this.props.location.pathname
+              }
+            />
+          </div>
+        </div>
+      </Layout>
+    )
+  }
 }
-
 
 export default PostTemplate
 
 export const pageQuery = graphql`
-query currentPostQuery($id: String!) {
+  query currentPostQuery($id: String!) {
     wordpressPost(id: { eq: $id }) {
-        title
-        content
-        featured_media{
-            localFile{
-                childImageSharp{
-                    resolutions{
-                        src
-                        width
-                        height
-                    }
-                }
+      title
+      content
+      featured_media {
+        localFile {
+          childImageSharp {
+            resolutions {
+              src
+              width
+              height
             }
+          }
         }
+      }
     }
     site {
-        siteMetadata {
-            title,
-            siteUrl
-        }
+      siteMetadata {
+        title
+        siteUrl
+      }
     }
-}
+  }
 `
