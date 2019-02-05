93444b9e1d74c4d657de81cc090d87d4e4456bf6
moetez <mo3tez@gmail.com>
2019-02-05 21:56:25 +0100

    style: add margin to the navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,30 +3,29 @@ import PropTypes from 'prop-types'
 import Header from './Header'
 import Footer from './Footer'
 import '../styles/index.scss'
-import 'font-awesome/css/font-awesome.min.css';
-import { StaticQuery, graphql } from "gatsby"
+import 'font-awesome/css/font-awesome.min.css'
+import { StaticQuery, graphql } from 'gatsby'
 
 const Layout = ({ children, data }) => (
-  
   <StaticQuery
-  query={graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `}
-  render={data => (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-        <Footer/>
-      </div>
-    </>
-  )}
+    `}
+    render={data => (
+      <>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div>
+          <main>{children}</main>
+          <Footer />
+        </div>
+      </>
+    )}
   />
 )
 
