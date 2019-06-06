23d1afef0588795cd080d87ef1aa5b76bcb38f64
moetez <contact@moetez.me>
2019-06-06 13:18:17 +0100

    feat: add form validation

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,8 @@ import Layout from '../components/Layout'
 import Recaptcha from 'react-google-recaptcha'
 import { navigate } from 'gatsby'
 import SEO from '../components/seo'
-
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
 const RECAPTCHA_KEY = process.env.GATSBY_SITE_RECAPTCHA_KEY
 
 function encode(data) {
@@ -11,34 +12,22 @@ function encode(data) {
     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&')
 }
+const SignupSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  message: Yup.string().required('Message is required'),
+  recaptcha: Yup.string().required('I only talk with humans!'),
+})
 
 export default class Contact extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
-  }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  handleRecaptcha = value => {
-    this.setState({ 'g-recaptcha-response': value })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch(error => alert(error))
   }
 
   render() {
@@ -62,64 +51,112 @@ export default class Contact extends React.Component {
             </li>
           </ul>
           <p>Or use the form below</p>
-          <form
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            action="/message"
-            data-netlify-honeypot="bot-field"
-            data-netlify-recaptcha="true"
-            onSubmit={this.handleSubmit}
+          <Formik
+            initialValues={{
+              name: '',
+              email: '',
+              message: '',
+              recaptcha: '',
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={values => {
+              fetch('/', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: encode({
+                  'form-name': values.name,
+                  name: values.name,
+                  email: values.email,
+                  message: values.message,
+                }),
+              })
+                .then(() => navigate('/message'))
+                .catch(error => alert(error))
+            }}
           >
-            <noscript>
-              <p>This form won’t work with Javascript disabled</p>
-            </noscript>
-            <div className="field">
-              <div className="control">
-                <input
-                  className="input"
-                  type="text"
-                  placeholder="Your Name"
-                  name="name"
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
-            <div className="field">
-              <p className="control has-icons-left">
-                <input
-                  className="input"
-                  type="email"
-                  placeholder="name@name.com"
-                  name="email"
-                  onChange={this.handleChange}
-                />
-                <span className="icon is-small is-left">
-                  <i className="fa fa-envelope" />
-                </span>
-              </p>
-            </div>
-            <div className="field">
-              <label className="label">Message</label>
-              <div className="control">
-                <textarea
-                  className="textarea"
-                  name="message"
-                  onChange={this.handleChange}
-                />
-              </div>
-            </div>
+            {({ errors, touched, setFieldValue }) => (
+              <Form
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                data-netlify-recaptcha="true"
+              >
+                <div className="field">
+                  <div className="control">
+                    <Field
+                      name="name"
+                      className={
+                        errors.name && touched.name
+                          ? 'input is-danger'
+                          : 'input'
+                      }
+                      placeholder="Your Name"
+                    />
+                    {errors.name && touched.name ? (
+                      <div className="help is-danger">{errors.name}</div>
+                    ) : null}
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control has-icons-left">
+                    <Field
+                      name="email"
+                      type="email"
+                      className={
+                        errors.email && touched.email
+                          ? 'input is-danger'
+                          : 'input'
+                      }
+                      placeholder="name@name.com"
+                    />
+                    {errors.email && touched.email ? (
+                      <div className="help is-danger">{errors.email}</div>
+                    ) : null}
+                    <span className="icon is-small is-left">
+                      <i className="fa fa-envelope" />
+                    </span>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Message</label>
+                  <div className="control">
+                    <Field
+                      name="message"
+                      component="textarea"
+                      className={
+                        errors.message && touched.message
+                          ? 'textarea is-danger'
+                          : 'textarea'
+                      }
+                    />
+                    {errors.message && touched.message ? (
+                      <div className="help is-danger">{errors.message}</div>
+                    ) : null}
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <Recaptcha
+                      sitekey={RECAPTCHA_KEY}
+                      onChange={value => {
+                        setFieldValue('recaptcha', value)
+                      }}
+                    />
+                    {errors.recaptcha && touched.recaptcha ? (
+                      <div className="help is-danger">{errors.recaptcha}</div>
+                    ) : null}
+                  </div>
+                </div>
 
-            <Recaptcha
-              ref="recaptcha"
-              sitekey={RECAPTCHA_KEY}
-              onChange={this.handleRecaptcha}
-            />
-            <div className="control">
-              <button className="button is-primary">Send Message</button>
-            </div>
-          </form>
-          <br />
+                <div className="control">
+                  <button className="button is-primary" type="submit">
+                    Send Message
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </Layout>
     )
